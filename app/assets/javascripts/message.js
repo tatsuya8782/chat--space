a037a61acff1202103941823dc7528f6d52bfdb6
tatsuya8782 <oimoq13@yahoo.co.jp>
2018-06-26 17:36:30 +0900

    自動更新機能まで

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,74 @@
+$(function(){
+    function buildHTML(message){
+    var image = '';
+    if (message.image)`<img src ="${message.image}">`;
+    var html = `<div class="chat" data-message-id="${message.id}">
+                  <div class="chat__memmber">
+                    ${message.name}
+                  </div>
+                  <div class="chat__memmber--time">
+                    ${message.created_at}
+                  </div>
+                  <div class="chat__message">
+                    <p class="chat__message__content">
+                      ${message.content}
+                    </p>
+                  </div>
+                  <div class="chat__message__image">
+                    ${image}
+                  </div>
+                </div>`
+                return html;
+    }
+    function scroll(){
+    $('.body').animate({scrollTop:$('.body')[0].scrollHeight},'slow');
+  }
+    $('#new_message').on('submit', function(e){
+      e.preventDefault();
+      var formData = new FormData(this);
+      var url = $(this).attr('action')
+      $.ajax({
+        url: url,
+        type: "post",
+        data: formData,
+        dataType: 'json',
+        processData: false,
+        contentType: false,
+      })
+      .done(function(data){
+        if(data.length !== 0){
+        var html = buildHTML(data);
+        $('.messages').append(html);
+        $('.form__submit').prop("disabled", false);
+        $('#new_message')[0].reset();
+        scroll();
+       }
+     })
+      .fail(function(){
+       alert('error');
+     })
+    })
+
+  setInterval(function() {
+    if (location.pathname.match(/\/groups\/\d+\/messages/)){
+      var message_id = $('.chat').last().data('message-id');
+        $.ajax({
+          url: location.href,
+          data: { id: message_id},
+          dataType: 'json',
+        })
+        .done(function(data){
+            var insertHTML = '';
+            data.forEach(function(message){
+              insertHTML = buildHTML(message);
+
+              $('.messages').append(insertHTML);
+              scroll();
+            })
+        })
+        .fail(function(data) {
+          alert('自動更新に失敗しました！！')
+        });
+      }
+    } , 5000 );
+  })
