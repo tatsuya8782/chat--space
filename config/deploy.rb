14b3c4415d8e32282c096e4bfdd3ea9611120294
tatsuya8782 <oimoq13@yahoo.co.jp>
2018-06-26 18:50:30 +0900

    name変更

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.11.0"
 
-set :application, "Chat-Space"
-set :repo_url, "git@github.com:tatsuya8782/Chat-Space.git"
+set :application, "chat--space"
+set :repo_url, "git@github.com:tatsuya8782/chat--space.git"
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
